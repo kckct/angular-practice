6411ccd05c2138e6adb4b244e2aa2561b8bf12af
kckct <ss010359@gmail.com>
2017-07-25 22:07:58 +0800

    重構[app.component.spec.ts] 綠燈

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,35 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
 import { TitleComponent } from './title/title.component';
+import { DebugElement } from '@angular/core';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
+  let target: AppComponent;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         TitleComponent
       ],
-    }).compileComponents();
-  }));
+    });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    target = new AppComponent();
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should appear app-title', () => {
-    const element = fixture.debugElement.query(By.css('app-title')).nativeElement;
-    expect(element).toBeTruthy();
+  describe(`TitleComponent`, () => {
+    it('should use app-title in HTML', () => {
+      htmlElement = debugElement.query(By.css('app-title')).nativeElement;
+      expect(htmlElement).toBeTruthy();
+    });
   });
 });
